9bc2bc2aaa79fd722d4b053689983f626172acf0
Andrew Evtukhov <andrew.evtukhov@synesis.ru>
2020-07-10 13:04:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/histogram/AudioPlayerUtil.kt
@@ -0,0 +1,40 @@
+package com.example.histogram
+
+import android.content.Context
+import android.media.MediaPlayer
+import androidx.annotation.RawRes
+
+class AudioPlayerUtil {
+    private var mediaPlayer: MediaPlayer? = null
+
+    /**
+     *
+     */
+    fun stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer!!.release()
+            mediaPlayer = null
+        }
+    }
+
+    fun play(context: Context, @RawRes resId: Int, completedCallback: () -> Unit) {
+        stop()
+
+        MediaPlayer.create(context, resId)
+            .apply {
+                mediaPlayer = this
+
+                this.setOnCompletionListener {
+                    stop()
+                    completedCallback()
+                }
+
+                this.start()
+            }
+    }
+
+    /**
+     *
+     */
+    fun getAudioSessionId(): Int? = mediaPlayer?.audioSessionId
+}
---
+++ app/src/main/java/com/example/histogram/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.histogram
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_CODE = 4837
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startDemoButton.setOnClickListener {
+            if (checkAudioPermission()) {
+                startActivity(Intent(this@MainActivity, DemoActivity::class.java))
+            }
+            else {
+                requestAudioPermission()
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    private fun checkAudioPermission(): Boolean =
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+
+    /**
+     *
+     */
+    private fun requestAudioPermission() {
+        ActivityCompat.requestPermissions(this, Array(1) {Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_CODE)
+    }
+}
